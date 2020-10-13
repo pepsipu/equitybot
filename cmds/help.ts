a4bc602a34fcbfde2cad2937b6598246425b4684
Sammy Hajhamid <pokeso123@hotmail.com>
2020-10-12 23:33:32 -0700

    more epic sbhit

--- cmds/help.ts
+++ cmds/help.ts
@@ -1,13 +1,14 @@
 import { Client, Message } from 'discord.js';
+import { prefix } from '../config.json';
 
 export default {
   name: 'help',
   constraint: (): boolean => true,
   command: async (client: Client, message: Message, args: string[]) => {
-    message.channel.send('To look for recent tickers with Positive VWAP Divergence, type "!positiveVWAP"\n'
-      + 'To look for recent tickers that have Crossed above VWAP, type "!crossVWAP"\n'
-      + 'To look for recent tickers with Pre-market highs, type "!PMhigh"\n'
-      + 'To look for recent tickers with Market Cap(under 300mil), price(under 15), and pattern(wedge), type "!screener"\n'
-      + 'To look for recent tickers with Market Cap(under 300 mil), price(under 10), and lots of recent news, type "!news 0 100"');
+    message.channel.send('To look for recent tickers with Positive VWAP Divergence, type "as!positiveVWAP"\n'
+      + 'To look for recent tickers that have Crossed above VWAP, type "as!crossVWAP"\n'
+      + 'To look for recent tickers with Pre-market highs, type "as!PMhigh"\n'
+      + 'To look for recent tickers with Market Cap(under 300mil), price(under 15), and pattern(wedge), type "as!screener"\n'
+      + 'To look for recent tickers with Market Cap(under 300 mil), price(under 10), and lots of recent news, type "as!news 0 100"');
   },
 };
